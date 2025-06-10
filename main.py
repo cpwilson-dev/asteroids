c897beea7db40d463f7fcd0a028199f2699070fb
Chris Wilson <cpwilson87@outlook.com>
2025-06-10 07:32:00 +0100

    feat: add game loop

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from constants import *
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    running = True
+
+    while running:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+        pygame.display.update()
 
 if __name__ == "__main__":
     main()
