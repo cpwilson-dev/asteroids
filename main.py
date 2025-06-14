059d19cf0e23136276754f574ad5b751a1762f0a
Chris Wilson <cpwilson87@outlook.com>
2025-06-14 17:42:54 +0100

    feat: create SpaceRocks class

--- main.py
+++ main.py
@@ -1,39 +1,12 @@
 import pygame
-from constants import *
-from player import Player
 
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Pygame Test")
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
-
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
-
-    Player.containers = (updateable, drawable)
-
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
-    dt = 0
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-
-        updateable.update(dt)
-
-        screen.fill("black")
-
-        for obj in drawable:
-            obj.draw(screen)
-
-        pygame.display.flip()
-
-        dt = clock.tick(60) / 1000
-
-
-if __name__ == "__main__":
-    main()
-
+pygame.quit()
