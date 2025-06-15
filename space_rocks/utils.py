48ea2aa19980d2640f2527ba9b7f5382ee635449
Chris Wilson <cpwilson87@outlook.com>
2025-06-15 10:17:59 +0100

    feat: add asteroids

--- space_rocks/utils.py
+++ space_rocks/utils.py
@@ -1,3 +1,5 @@
+import random
+
 from pygame import Vector2
 from pygame.image import load
 
@@ -16,3 +18,15 @@ def wrap_position(position, surface):
     x, y = position
     w, h = surface.get_size()
     return Vector2(x % w, y % h)
+
+
+def get_random_position(surface):
+    return Vector2(
+        random.randrange(surface.get_width()), random.randrange(surface.get_height())
+    )
+
+
+def get_random_velocity(min_speed, max_speed):
+    speed = random.randint(min_speed, max_speed)
+    angle = random.randrange(0, 360)
+    return Vector2(speed, 0).rotate(angle)
